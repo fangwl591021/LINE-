cff74babc904bad6bb54d1b9128f3f74c8821d13
fangwl591021 <50135766+fangwl591021@users.noreply.github.com>
2026-08-16 16:04:47 +0800

    Load card quota runtime preflight

--- js/modules/settings.js
+++ js/modules/settings.js
@@ -17,13 +17,15 @@
   }
 
   var urls = [
-    'js/modules/settings-core.js?v=20260816-cardquota1',
-    'js/modules/settings-card-quota.js?v=20260816-cardquota1'
+    'js/modules/settings-core.js?v=20260816-cardquota2',
+    'js/modules/settings-card-quota.js?v=20260816-cardquota2',
+    'js/modules/settings-card-quota-runtime.js?v=20260816-cardquota2'
   ];
 
   if (document.readyState === 'loading' && document.currentScript) {
     document.write('<script src="' + urls[0] + '"><\/script>');
     document.write('<script src="' + urls[1] + '"><\/script>');
+    document.write('<script src="' + urls[2] + '"><\/script>');
   } else {
     loadSequentially(urls).catch(function (error) {
       console.error('[settings-loader]', error);
